1d83671f0bd141bc620b7988336adbdc576f2888
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-19 21:45:00 +0000

    javascript operation

---
+++ 01-basic/04-operation.js
@@ -0,0 +1,31 @@
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************ operrations ***********
+
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
