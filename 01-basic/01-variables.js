b78bf74ca9e770990ecdc847147ae3c21afe56ce
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-19 18:25:21 +0000

    datatypes

---
+++ 01-basic/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "rehan@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+// accountId = 2
+
+accountEmail = "hd@.com"
+accountPassword = "2432"
+accountCity = "pindi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
