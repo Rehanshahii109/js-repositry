58701b9ca1f75d32a3bca1820a4a01efdc8a5461
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-19 21:17:07 +0000

    converstion of datatypes

---
+++ 01-basic/03-converstion.js
@@ -0,0 +1,27 @@
+ let score = "null"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ //"33" => 33
+ // "33ab" => nan
+ // true =>; false => 0
+
+ let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+
+  let someNumber = 33
+   
+  let stringNumber = String(someNumber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
+ 
