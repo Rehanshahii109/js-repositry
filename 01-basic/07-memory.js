7746439345484e9f697fa197600ed3cd950d50ba
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-19 22:59:59 +0000

    memory of js

---
+++ 01-basic/07-memory.js
@@ -0,0 +1,27 @@
+// stack, heap  
+
+
+//stack(primitive)
+
+//heap(non-primitive)
+
+
+let myYoutubename = "rehan"
+
+let anothername = myYoutubename
+anothername = "chai aur code"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let user = {
+  email: "hddhd@.com",
+  upi: "user@bj"
+
+}
+let userTwo = userOne
+
+userTwo.email = "rehd@.com"
+
+console.log(userOne.email);
+console.log(user.Two.email);
