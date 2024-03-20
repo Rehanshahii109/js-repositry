454e6fe53117b9240a08f6f61c49fc570dd87201
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-20 00:40:08 +0000

    string methods of js

---
+++ 01-basic/08-string.js
@@ -0,0 +1,42 @@
+const name = "rehan"
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+
+console.log('hello my name is ${rehan} and my repo count is ${50}');
+
+const gameName = new String('rehan')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+//console.log(gameName.Lenght);
+//console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newString = " rehan  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://rehan.com/rehan%20shah"
+
+url.replace('%20', '-')
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
