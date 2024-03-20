4996bbe76aa125060ce87a8ac3b35255ea777e35
Rehanshahii109 <149240080+Rehanshahii109@users.noreply.github.com>
2024-03-20 23:54:41 +0000

    json format

---
+++ 02-basic/05-destructure.js
@@ -0,0 +1,27 @@
+
+
+// object desture
+
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "rehan"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+// const navbar = (company) => {
+
+// }
+
+// navbar(company = "rehan")
+  
+//  {
+//   "name": "rehan",
+//   coursename: "js in english",
+//   price: "free"
+//  }
+
